763515b35de8a5e9b036b819bbd8b2088575754a
Eugene Petrik <eugeniy.petrik@gmail.com>
2021-07-25 21:29:52 +0300

    Add playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,89 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from 'playwright';
+import * as dotenv from 'dotenv';
+import { env, timeouts } from './src/configs';
+
+dotenv.config({ path: __dirname + '/.env' });
+
+const config: PlaywrightTestConfig = {
+  testDir: 'src/tests',
+
+  testMatch: 'src/tests/**/*.spec.ts',
+
+  timeout: timeouts.test,
+
+  retries: env.RETRIES,
+  workers: env.WORKERS,
+
+  use: {
+    launchOptions: {
+      slowMo: env.SLOW_MO,
+    },
+
+    baseURL: env.APP_URL,
+
+    headless: env.HEADLESS,
+    ignoreHTTPSErrors: env.IGNORE_HTTP_ERRORS,
+
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'Chromium',
+      use: {
+        browserName: 'chromium',
+        viewport: {
+          width: env.VIEWPORT_WIDTH,
+          height: env.VIEWPORT_HEIGHT,
+        },
+      },
+    },
+    {
+      name: 'Firefox',
+      use: {
+        browserName: 'firefox',
+        viewport: {
+          width: env.VIEWPORT_WIDTH,
+          height: env.VIEWPORT_HEIGHT,
+        },
+      },
+    },
+    {
+      name: 'WebKit',
+      use: {
+        browserName: 'webkit',
+        viewport: {
+          width: env.VIEWPORT_WIDTH,
+          height: env.VIEWPORT_HEIGHT,
+        },
+      },
+    },
+    {
+      name: 'Pixel 4',
+      use: {
+        locale: 'fr-FR',
+        geolocation: { longitude: 48.858455, latitude: 2.294474 },
+        permissions: ['geolocation'],
+
+        browserName: 'chromium',
+        ...devices['Pixel 4'],
+      },
+    },
+    {
+      name: 'iPhone 11',
+      use: {
+        locale: 'fr-FR',
+        geolocation: { longitude: 48.858455, latitude: 2.294474 },
+        permissions: ['geolocation'],
+
+        browserName: 'webkit',
+        ...devices['iPhone 11'],
+      },
+    },
+  ],
+};
+
+export default config;
