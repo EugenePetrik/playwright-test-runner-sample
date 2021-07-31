5df070f6c0840746ca1e469f957181303b418427
Eugene Petrik <eugeniy.petrik@gmail.com>
2021-07-31 15:43:27 +0300

    Add test for Create a new article - snapshots

---
+++ src/tests/snapshots/article.create.article.snashots.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import faker from 'faker';
+import { NewArticlePage } from '../../pageobjects/article';
+import { createUser, signInUser } from '../../utils/api';
+import type { IUser } from '../../utils/types';
+import userResponse from '../../data/mock/user.json';
+
+test.describe('Create a new article - snapshots', () => {
+  let newArticlePage: NewArticlePage;
+
+  const user: IUser = {
+    email: `test${new Date().getTime() / 1000}@example.com`,
+    password: faker.internet.password(),
+    username: faker.internet.userName().replace(/[_'.]/g, '').toLowerCase().slice(0, 10),
+  };
+
+  test.beforeAll(async () => {
+    await createUser(user);
+  });
+
+  test.beforeEach(async ({ page }) => {
+    newArticlePage = new NewArticlePage(page);
+
+    await signInUser(page, user);
+    await newArticlePage.mockUserResponse(userResponse);
+    await newArticlePage.open();
+  });
+
+  test('should open the page', async ({ page }) => {
+    expect(await page.screenshot()).toMatchSnapshot('create-article-page.png', { threshold: 0.1 });
+  });
+});
