1e602db6b252f5e0981fa77732c196f70a5425f1
Eugene Petrik <eugeniy.petrik@gmail.com>
2021-07-31 14:15:38 +0300

    Add test for Sign up page - snapshots

---
+++ src/tests/snapshots/sign.up.snapshots.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+import { SignUpPage } from '../../pageobjects/sign.up.page';
+
+test.describe('Sign up page - snapshots', () => {
+  let signUpPage: SignUpPage;
+
+  test.beforeEach(async ({ page }) => {
+    signUpPage = new SignUpPage(page);
+    await signUpPage.open();
+  });
+
+  test('should open the page', async ({ page }) => {
+    expect(await page.screenshot()).toMatchSnapshot('sign-up-page.png', { threshold: 0.1 });
+  });
+});
